e9b53c3c47e9fcf347157fa6106c00e9cffc5403
LuthfiAhmad1 <144640413+LuthfiAhmad1@users.noreply.github.com>
2023-09-11 09:39:15 +0700

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main( ){
+      int a,b,c;
+       cout << "PEMROGRAM OPERASI PEBJUMLAHAN \n";
+      cout<<"masukan angka pertama: ";
+      cin>>a;
+      cout<<"masukan angka kedua ";
+      cin>>b;
+      c=a+b;
+      cout<<"hasilnya adalah: "<< c << endl;
+       return 0;
+}
